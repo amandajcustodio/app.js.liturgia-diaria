f522eb8bbc45f5a5ea19de6d1a1232bca8472020
amandajcustodio <amandajcustodio@outlook.com>
2026-04-20 17:15:49 -0300

    fix(config): set correct url

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -1,5 +1,5 @@
-// export const baseUrl = "https://api-nodejs-liturgia-diaria.vercel.app";
-export const baseUrl = "http://localhost:3000";
+export const baseUrl = "https://api-nodejs-liturgia-diaria.vercel.app";
+// export const baseUrl = "http://localhost:3000";
 
 export const endpoints = {
   todayMissallete: `${baseUrl}/missallete/today`,
